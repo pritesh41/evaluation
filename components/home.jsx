aefc2bf0897f892daf3a600f2ab19f40ba416088
Pritesh <149871904+pritesh41@users.noreply.github.com>
2025-09-20 12:47:55 +0530

    Add files via upload

---
+++ components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {link} from 'react-router-dom'
+
+function home() {
+  return (
+    <div>
+        <input type="text" />
+      <a href="">Submit</a>
+    </div>
+  )
+}
+
+export default home
